d42499795c8d87d580f410fa18e256bfc912d578
Lin Zhao <linzhao@uri.edu>
2022-07-22 14:10:50 -0400

    add: teleoperation behavior

---
+++ bhv_teleoperation/src/teleoperation/teleoperation.cpp
@@ -0,0 +1,180 @@
+#include "teleoperation.h"
+#include "pluginlib/class_list_macros.h"
+
+using namespace helm;
+
+Teleoperation::Teleoperation() 
+  : m_use_joy(false), m_last_yaw (false), m_last_pitch(false) {
+    std::cout << "A message from the teleoperation" << std::endl;
+}
+
+
+Teleoperation::~Teleoperation() {
+
+    m_joy_sub.shutdown();
+
+}
+
+void Teleoperation::initialize() {
+
+    /**
+     * @brief Initialize node handler with the behavior namespace
+     *
+     * @details Parameters for the behavior is loaded under
+     * /helm/<behavior_name> namespace. Therefore, nodehandler must use that
+     * name as well or should take that namespace into account when reading the
+     * parameters.
+     *
+     * @note variables with redundant class names are used for emphesizing the
+     * base class member variables.
+     */
+    m_pnh.reset(
+        new ros::NodeHandle(ros::this_node::getName() + "/" +
+            BehaviorBase::m_name)
+    );
+
+    m_nh.reset(new ros::NodeHandle(""));
+
+    // ROS related: load parameters, setup sub/pub
+
+    m_pnh->param<double>("max_surge", m_max_surge, 1.0);
+    m_pnh->param<double>("max_pitch_rate", m_max_pitch_rate, 3.15);
+    m_pnh->param<double>("max_yaw_rate", m_max_yaw_rate, 3.15);
+    m_pnh->param<std::string>("joy_topic", m_joy_topic_name, "joy");
+    m_pnh->param<int>("joy_map/axes_surge", m_axes_surge, 1);
+    m_pnh->param<int>("joy_map/axes_pitch", m_axes_pitch, 4);
+    m_pnh->param<int>("joy_map/axes_yaw", m_axes_yaw, 3);
+    m_pnh->param<int>("joy_map/button_enable", m_button_enable, 5);
+
+
+    m_joy_sub = m_nh->subscribe(m_joy_topic_name, 100, &Teleoperation::f_joy_cb, this);
+
+    /**
+     * @brief Declare the degree of freedoms to be controlled by the behavior
+     *
+     * @details This member variable dictates the DOFs that can be controllable
+     * by the behavior. If this member is not initialized, behavior can only
+     * trigger state changes. This vector gets values from enum type of
+     * seal_msgs/ControlMode enums.
+     *
+     */
+    BehaviorBase::m_dofs = decltype(m_dofs){
+        // for flight mode
+        seal_msgs::ControlMode::DOF_SURGE,
+        seal_msgs::ControlMode::DOF_PITCH,
+        seal_msgs::ControlMode::DOF_YAW,
+
+        // for another control mode
+        seal_msgs::ControlMode::DOF_SURGE,
+        seal_msgs::ControlMode::DOF_PITCH_RATE,
+        seal_msgs::ControlMode::DOF_YAW_RATE,
+    };
+}
+
+void Teleoperation::f_joy_cb(const sensor_msgs::Joy::ConstPtr &m) {
+    // grab control value from joystick
+    m_joy_surge = m->axes[m_axes_surge];
+    m_joy_yaw_rate = m->axes[m_axes_yaw];
+    m_joy_pitch_rate = m->axes[m_axes_pitch];
+
+    // open or close joystick: RB
+    if (m->buttons[m_button_enable] == 1 ) {
+        // first time enable joystick and record vehicle pose
+        if(!m_use_joy) {
+            // record global information
+            m_recorded_picth = BehaviorBase::m_process_values.orientation.y;
+            m_recorded_yaw = BehaviorBase::m_process_values.orientation.z;
+        }
+
+        m_use_joy = true;
+
+        // only record pose if pitch input decrease to 0
+        if(m_joy_pitch_rate == 0) {
+            if(m_last_pitch) {
+                // record global information
+                m_recorded_picth = BehaviorBase::m_process_values.orientation.y;
+                m_last_pitch = false;
+            }
+        }
+        else {
+            m_last_pitch = true;
+        }
+
+        // only record pose if yaw input decrease to 0
+        if(m_joy_yaw_rate == 0) {
+            if(m_last_yaw) {
+                // record global information
+                m_recorded_yaw = BehaviorBase::m_process_values.orientation.z;
+                m_last_yaw = false;
+            }
+        }
+        else {
+            m_last_yaw = true;
+        }
+    }
+    else {
+        m_use_joy = false;
+    }
+}
+
+
+void Teleoperation::activated() {
+    /**
+     * @brief This function is called when the behavior gets activated.
+     *
+     * @note This function is an inherited virtual function. It may left
+     * unimplemented.
+     *
+     * @details This function is called when the behavior internal state
+     * defined by #BehaviorBase::m_actived changes to true.
+     */
+    std::cout << "teleoperation behavior is activated!" << std::endl;
+}
+
+void Teleoperation::disabled() {
+    /**
+     * @brief This function is called when the behavior gets disabled.
+     *
+     * @note This function is an inherited virtual function. It may left
+     * unimplemented.
+     *
+     * @details This function is called when the behavior internal state
+     * defined by #BehaviorBase::m_actived changes to false.
+     */
+    std::cout << "teleoperation behavior is disabled!" << std::endl;
+}
+
+//! NOTE: for the pitch and yaw, we can't direct assign the joystick value as desired_value,
+//!       because pitch and yaw are in the global frame, but surge is ok. it's in the body frame.
+bool Teleoperation::request_set_point(
+    seal_msgs::ControlProcess *set_point) {
+
+    if( !m_use_joy ) {
+        return false;
+    }
+
+    // get surge input
+    double surge_rate = m_max_surge * m_joy_surge.load(std::memory_order_relaxed);
+    // get pitch input
+    double pitch_rate = m_max_pitch_rate * m_joy_pitch_rate.load(std::memory_order_relaxed);
+    double pitch_angle = pitch_rate * (1.0 / BehaviorBase::m_helm_frequency);
+    // get yaw input
+    double yaw_rate = m_max_yaw_rate * m_joy_yaw_rate.load(std::memory_order_relaxed);
+    double yaw_angle = yaw_rate * (1.0 / BehaviorBase::m_helm_frequency);
+
+    // Set body frame velocity 
+    set_point->velocity.x = surge_rate;
+    set_point->angular_rate.y = pitch_rate;
+    set_point->angular_rate.z = - yaw_rate;
+
+    // Set global frame orientation angles
+    set_point->orientation.y =  m_recorded_picth + pitch_angle;
+    set_point->orientation.z =  m_recorded_yaw - yaw_angle;
+
+    return true;
+}
+
+/**
+ * @brief Behavior must export the class to the Plugin library.
+ */
+PLUGINLIB_EXPORT_CLASS(helm::Teleoperation, helm::BehaviorBase)
---
+++ bhv_teleoperation/src/teleoperation/teleoperation.h
@@ -0,0 +1,171 @@
+#pragma once
+
+#include "atomic"
+
+#include "ros/ros.h"
+#include "std_msgs/Float64.h"
+#include "sensor_msgs/Joy.h"
+
+#include "behavior_interface/behavior_base.h"
+#include "seal_msgs/ControlProcess.h"
+
+namespace helm {
+
+    class Teleoperation : public BehaviorBase {
+    private:
+        /**
+         * @brief Destroy the Teleoperation object
+         */
+        ~Teleoperation() override;
+
+        /**
+         * @brief Initialize function
+         *
+         * @details This function initializes the behavior. It is called by the
+         * helm. It is a pure virtual function and it must be implemented in the
+         * behavior. If left unimplemented, code will not compile. In this
+         * function, user is responsible for proper initalization of the
+         * behavior. This function must be unblocking, otherwise, every other
+         * behavior may wait this function to return. In this function user
+         * should create #ros::NodeHandle, and define controlled degrees of
+         * freedom. Below is a trivial implementation of this function
+         *
+         */
+        void initialize() override;
+
+        /**
+         * @brief trivial node handler for load parameters
+         */
+        ros::NodeHandlePtr m_pnh;
+
+        /**
+         * @brief trivial node handler for subscribe and publish
+         */
+        ros::NodeHandlePtr m_nh;
+
+        /**
+         * @brief This function is inherited from #BehaviorBase
+         */
+        void activated() override;
+
+        /**
+         * @brief This function is inherited from #BehaviorBase
+         */
+        void disabled() override;
+
+        /**
+         * @brief Joystick subscriber
+         */
+        ros::Subscriber m_joy_sub;
+
+        /**
+         * @brief Trivial joy stick callback
+         * 
+         * @details 1)record joystick input; 2)record gloabl information
+         *          (e.g. pitch and yaw angle) if it's trigged by Joystick Enabled 
+         *          or Orientation Update by Joystick    
+         * 
+         * @param m Message
+         */
+        void f_joy_cb(const sensor_msgs::Joy::ConstPtr &m);
+
+        /**
+         * @brief Joystick value for surge (velocity in body frame x-axis) 
+         */
+        std::atomic<double> m_joy_surge;
+
+        /**
+         * @brief Joystick value for yaw angular velocity 
+         */
+        std::atomic<double> m_joy_yaw_rate;
+
+        /**
+         * @brief Joystick value for pitch angular velocity 
+         */
+        std::atomic<double> m_joy_pitch_rate;
+
+        /**
+         * @brief Max surge value from joystick input
+         */
+        double m_max_surge;
+
+        /**
+         * @brief Max picth rate value from joystick input
+         */    
+        double m_max_pitch_rate;
+
+        /**
+         * @brief Max yaw rate value from joystick input
+         */        
+        double m_max_yaw_rate;
+
+        /**
+         * @brief ROS rostopic for joystick node
+         */   
+        std::string m_joy_topic_name;
+
+        /**
+         * @brief Joystick map for surge control stick
+         */   
+        int m_axes_surge;
+
+        /**
+         * @brief Joystick map for pitch control stick
+         */           
+        int m_axes_pitch;
+
+        /**
+         * @brief Joystick map for yaw control stick
+         */   
+        int m_axes_yaw;
+
+        /**
+         * @brief Joystick map for enable button
+         */   
+        int m_button_enable;
+
+        /**
+         * @brief Value to indicate joystick is enabled or not
+         */          
+        std::atomic<bool> m_use_joy;
+
+        /**
+         * @brief Value to indicate joystick has pitch input previously
+         */ 
+        bool m_last_pitch;
+
+        /**
+         * @brief Value to indicate joystick has yaw input previously
+         */         
+        bool m_last_yaw;
+
+        /**
+         * @brief Value of recorded picth angle in global frame
+         */   
+        std::atomic<double> m_recorded_picth;
+
+        /**
+         * @brief Value of recorded yaw angle in global frame
+         */ 
+        std::atomic<double> m_recorded_yaw;
+
+    public:
+
+        /**
+         * @brief Trivial constructor
+         */
+        Teleoperation();
+
+        /**
+         * @brief Request set point from the behavior. It is consumed by helm.
+         *
+         * @param msg Result value of the behavior. This value is written by the
+         *            Behavior. Helm uses this variable to generate set_point
+         *            for the controller.
+         * @return true if you want helm to use the result.
+         * @return false if you don't want helm to use the result.
+         */
+        bool request_set_point(seal_msgs::ControlProcess *msg) override;
+
+    };
+}
